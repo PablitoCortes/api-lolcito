eb5d11500cddb5713fc449fbd5a81625d88d8e96
PablitoCortes <cjuan1597@gmail.com>
2024-09-02 18:23:27 -0500

    Agregar html estatico
    Que: se agregó una ruta a la raiz que muestra un titulo de html
    Porque: Para tener mejor visivilidad al deployar proyecto a vercel

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -5,5 +5,8 @@ const router = Router();
 
 //router recibe la solicitud del server y decide a donde enviarla con el enrutador de users, o de posts.
 router.use("/champs", ChampsRouter);
+router.use("/", (req, res) => {
+  res.status(200).send(`<h1> hola madafakas </h1>`);
+});
 
 module.exports = router;
